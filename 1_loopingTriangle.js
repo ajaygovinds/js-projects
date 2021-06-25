4176ceb87f23d17557b06291858573b3a81e4c50
Ajay Govind <ajaygovin@gmail.com>
2021-06-25 07:27:45 +0400

    Comments for 1_loopingTriangle.js

--- 1_loopingTriangle.js
+++ 1_loopingTriangle.js
@@ -1,3 +1,6 @@
+// this program prints a left-aligned right angled triangle having height of the value provided as a input
+// execute this code by 'node 1_loopingTriangle.js <YOUR_INPUT_VALUE>'
+
 let count = process.argv[2]; //to cli input, provided without whitespaces
 let toPrint = '';
 let stepCount = 1;
